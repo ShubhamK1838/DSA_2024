7cfe961bb7c4b4067a5cf5f87fe22d97ab423539
ShubhamK1838 <jadoo.8k@gmail.com>
2024-01-10 22:20:10 +0530

    10-1-24

---
+++ _1_IsPowerOf2.cpp
@@ -0,0 +1,35 @@
+// to check the given number is power of 2 or not 
+
+#include<iostream> 
+
+
+using namespace std; 
+
+bool isPowerOfTwo(int n)
+{
+
+    
+    while(n>1)
+    {
+        if(n%2 !=0)
+        {
+            return false; 
+        }
+         n/=2; 
+    }
+
+    return true; 
+}
+
+int main()
+{
+
+    int n; 
+    cout<<"Enter the number :"; 
+    cin>>n; 
+
+    cout<<"\nResult : "<<(isPowerOfTwo(n)==1 ? "TRUE": "FALSE"); 
+
+
+    return 0; 
+}
