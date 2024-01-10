232c0cc229fec07d050244887ed7a1db82be6f50
ShubhamK1838 <jadoo.8k@gmail.com>
2024-01-10 23:36:26 +0530

    10-1-24

---
+++ questions/_4_startPattern.cpp
@@ -0,0 +1,25 @@
+// prints the number n X n star pattern 
+
+#include<iostream > 
+
+
+using namespace std; 
+
+
+int main()
+{
+
+    int n; 
+    cin>>n; 
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++ )
+        {
+            cout<<"*"; 
+        } ; 
+        cout<<"\n"; 
+        
+    }
+    
+}
